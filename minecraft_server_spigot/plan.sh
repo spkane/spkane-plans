5e5c639fcdb28ec0e5af59c4539bfeb28608ddb6
Sean P. Kane <skane@newrelic.com>
2018-12-23 19:37:27 -0800

    Update for 1.12.2

--- minecraft_server_spigot/plan.sh
+++ minecraft_server_spigot/plan.sh
@@ -1,12 +1,12 @@
 pkg_name=minecraft_server_spigot
 pkg_origin=spkane
-pkg_version=1.10.2
+pkg_version=1.12.2
 pkg_maintainer="Sean P. Kane <spkane00@gmail.com>"
 pkg_license=('Minecraft End User License Agreement')
 pkg_source=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 pkg_filename=BuildTools.jar
 pkg_build_deps=(core/jre8 core/git core/bash)
-pkg_deps=(core/glibc core/server-jre core/curl core/bash)
+pkg_deps=(core/jre8 core/curl core/bash)
 pkg_upstream_url=https://www.spigotmc.org/
 pkg_interpreters=(bin/bash)
 pkg_svc_user=root
@@ -24,7 +24,7 @@ do_verify() {
 do_build() {
   mkdir -p $(pkg_path_for core/git)/etc 
   $(pkg_path_for core/git)/bin/git config --global core.autocrlf false
-  $(pkg_path_for core/jre8)/bin/java -jar ../${pkg_filename} --rev ${pkg_version}
+  $(pkg_path_for core/jre8)/bin/java -Xmx2048M -jar ../${pkg_filename} --rev ${pkg_version}
   return $?
 }
 
